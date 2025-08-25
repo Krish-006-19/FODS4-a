3dc22d3e85934e2e3aff9629aa995c2d5387375d
Krish Ramena <krish.ramena24@vit.edu>
2025-08-25 10:18:31 +0530

    Update classesnobjs2.cpp

--- classesnobjs2.cpp
+++ classesnobjs2.cpp
@@ -1,71 +1,151 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
-class Student{
-
-private:
-    double* balance;
-    string password;
+class Node {
 public:
-    int* age;
-    // unparameterized Constructor
-    Student(){ cout<<"Hi, I am constructor!\n"; }
-    // parameterized Constructor
-    Student(string accId, string userName, int Age){
-        cout<<"Hi, I am constructor!\n";
-        this->accId = accId;
-        this->userName = userName;
-        age = new int;
-        *age = Age;
-    }
-    // Custom Copy Constructor
-    Student(Student &ref){
-        // ref is a object
-        /*
-        let ref = {
-        accId:"xyz101",
-        userName:"Shivam Shinde",
-        age:"10921XLo9"
-        }
-        *(ref.age) = val
-        ref.age = address
-        */
-        cout<<"Copy constructor called!\n";
-        this->accId = ref.accId;
-        this->userName = ref.userName;
-        // Shallow Copy
-        // this->age = age;
-        
-        // Deep Copy
-        age = new int;
-        *age = *(ref.age);
-    }
-    string accId;
-    string userName;
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
 
-    void print(){
-    cout<<accId<<endl<<userName<<endl<<*age<<endl;
+class Stack {
+    Node* top;  // pointer to the top node
+public:
+    Stack() : top(nullptr) {}
+
+    void push(int x) {
+        Node* newNode = new Node(x);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+        int val = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return val;
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+        return top->data;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is Empty" << endl;
+            return;
+        }
+        Node* temp = top;
+        cout << "[";
+        while (temp) {
+            cout << temp->data;
+            if (temp->next) cout << ",";
+            temp = temp->next;
+        }
+        cout << "]" << endl;
+    }
+
+    ~Stack() {
+        while (!isEmpty()) pop();
+    }
+};
+class Queue {
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() : front(nullptr), rear(nullptr) {}
+
+    void insert(int x) {
+        Node* newNode = new Node(x);
+        if (rear == nullptr) { // empty queue
+            front = rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    int remove() {
+        if (isEmpty()) {
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        int val = front->data;
+        Node* temp = front;
+        front = front->next;
+        if (front == nullptr) rear = nullptr;
+        delete temp;
+        return val;
+    }
+
+    int getFront() {
+        if (isEmpty()) {
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        return front->data;
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Queue is Empty" << endl;
+            return;
+        }
+        Node* temp = front;
+        cout << "[";
+        while (temp) {
+            cout << temp->data;
+            if (temp->next) cout << ",";
+            temp = temp->next;
+        }
+        cout << "]" << endl;
+    }
+
+    ~Queue() {
+        while (!isEmpty()) remove();
     }
 };
 
-int main(){
-    Student stu; // unparameterized
-    Student stu1("123Hgkhu","Ram Prem Chopra",25); // parameterized
-    Student stu2(stu1); // custom copy constructor
+int main() {
+    Stack s;
+    s.push(4);
+    s.push(3);
+    s.push(2);
+    s.display();
 
-    /*  There are two types of copy
-    1) shallow copy -> fails when -> Dynamic Memory Allocation
-    2) Deep copy
-    */
+    cout << "Popped from Stack: " << s.pop() << endl;
+    s.display();
 
-    // Problems with shallow copy
-    // solution => deep copy
-    stu1.print();
-    *(stu2.age) = 35;
-    stu1.print();
-    stu2.userName = "Shalu Sharma";
-    stu2.print();  
-    cout<<"\n";
+    Queue q;
+    q.insert(10);
+    q.insert(20);
+    q.insert(30);
+    q.display();
+
+    q.remove();
+    q.display();
+
+    q.insert(40);
+    q.insert(50);
+    q.display();
+
     return 0;
 }
