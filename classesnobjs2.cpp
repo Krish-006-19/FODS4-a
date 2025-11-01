1603931ba424250fdc561f8e4c1ea0c5df9582ad
Krish Ramena <krish.ramena24@vit.edu>
2025-11-01 13:47:41 +0530

    Update classesnobjs2.cpp

--- classesnobjs2.cpp
+++ classesnobjs2.cpp
@@ -1,153 +1,196 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Node {
 public:
-    int data;
+    int val;
     Node* next;
-    Node(int val) : data(val), next(nullptr) {}
+
+    Node(int val) {
+        this->val = val;
+        next = nullptr;
+    }
 };
 
 class Stack {
-    Node* top;  
+    Node* head;
 public:
-    Stack() : top(nullptr) {}
-
-    void push(int x) {
-        Node* newNode = new Node(x);
-        newNode->next = top;
-        top = newNode;
+    Stack() {
+        head = nullptr;
     }
-
+    void push(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+    }
     int pop() {
         if (isEmpty()) {
-            cout << "Stack is Empty" << endl;
+            cout << "Stack is Empty\n";
             return -1;
         }
-        int val = top->data;
-        Node* temp = top;
-        top = top->next;
+        Node* temp = head;
+        int val = head->val;
+        head = head->next;
         delete temp;
         return val;
     }
-
     int peek() {
         if (isEmpty()) {
-            cout << "Stack is Empty" << endl;
+            cout << "No data available\n";
             return -1;
         }
-        return top->data;
+        return head->val;
     }
-
     bool isEmpty() {
-        return top == nullptr;
+        return head == nullptr;
     }
-
     void display() {
-        if (isEmpty()) {
-            cout << "Stack is Empty" << endl;
+        Node* temp = head;
+        if (!temp) {
+            cout << "Stack is Empty\n";
             return;
         }
-        Node* temp = top;
-        cout << "[";
+        cout << "Stack elements: ";
         while (temp) {
-            cout << temp->data;
-            if (temp->next) cout << ",";
+            cout << temp->val << " ";
             temp = temp->next;
         }
-        cout << "]" << endl;
-    }
-
-    ~Stack() {
-        while (!isEmpty()) pop();
+        cout << "\n";
     }
 };
+
 class Queue {
     Node* front;
     Node* rear;
-
 public:
-    Queue() : front(nullptr), rear(nullptr) {}
-
-    void insert(int x) {
-        Node* newNode = new Node(x);
-        if (rear == nullptr) { 
+    Queue() {
+        front = rear = nullptr;
+    }
+    void push(int val) {
+        Node* newNode = new Node(val);
+        if (!rear) {
             front = rear = newNode;
             return;
         }
         rear->next = newNode;
         rear = newNode;
     }
-
-    int remove() {
+    int pop() {
         if (isEmpty()) {
-            cout << "Queue is Empty" << endl;
+            cout << "Queue is Empty\n";
             return -1;
         }
-        int val = front->data;
         Node* temp = front;
+        int val = front->val;
         front = front->next;
-        if (front == nullptr) rear = nullptr;
+        if (!front) rear = nullptr;
         delete temp;
         return val;
     }
-
-    int getFront() {
+    int peek() {
         if (isEmpty()) {
-            cout << "Queue is Empty" << endl;
+            cout << "Queue is Empty\n";
             return -1;
         }
-        return front->data;
+        return front->val;
     }
-
     bool isEmpty() {
         return front == nullptr;
     }
-
     void display() {
-        if (isEmpty()) {
-            cout << "Queue is Empty" << endl;
+        Node* temp = front;
+        if (!temp) {
+            cout << "Queue is Empty\n";
             return;
         }
-        Node* temp = front;
-        cout << "[";
+        cout << "Queue elements: ";
         while (temp) {
-            cout << temp->data;
-            if (temp->next) cout << ",";
+            cout << temp->val << " ";
             temp = temp->next;
         }
-        cout << "]" << endl;
-    }
-
-    ~Queue() {
-        while (!isEmpty()) remove();
+        cout << "\n";
     }
 };
 
 int main() {
-    cout << "Stack"<<endl;
-    Stack s;
-    s.push(4);
-    s.push(3);
-    s.push(2);
-    s.display();
-
-    cout << "Popped from Stack: " << s.pop() << endl;
-    s.display();
-    cout<< "============================================"<<endl;
-    cout << "Queue"<<endl;
+    string choice, subchoice;
+    Stack st;
     Queue q;
-    q.insert(10);
-    q.insert(20);
-    q.insert(30);
-    q.display();
+    int input;
+    bool flag = false;
 
-    q.remove();
-    q.display();
+    while (!flag) {
+        cout << "\nChoose:\n1. Stack\n2. Queue\n3. Exit\n";
+        cin >> choice;
 
-    q.insert(40);
-    q.insert(50);
-    q.display();
+        if (choice.length() != 1 || choice[0] < '1' || choice[0] > '3') {
+            cout << "Please Enter A Valid Number\n";
+            continue;
+        }
+
+        switch (choice[0]) {
+        case '1':
+            while (true) {
+                cout << "\nStack Menu:\n1. Push\n2. Pop\n3. Display\n4. Exit\n";
+                cin >> subchoice;
+
+                if (subchoice.length() != 1 || subchoice[0] < '1' || subchoice[0] > '4') {
+                    cout << "Please Enter A Valid Number\n";
+                    continue;
+                }
+
+                if (subchoice[0] == '4') break;
+
+                switch (subchoice[0]) {
+                case '1':
+                    cout << "Enter value: ";
+                    cin >> input;
+                    st.push(input);
+                    break;
+                case '2':
+                    cout << "Popped: " << st.pop() << "\n";
+                    break;
+                case '3':
+                    st.display();
+                    break;
+                }
+            }
+            break;
+
+        case '2':
+            while (true) {
+                cout << "\nQueue Menu:\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n";
+                cin >> subchoice;
+
+                if (subchoice.length() != 1 || subchoice[0] < '1' || subchoice[0] > '4') {
+                    cout << "Please Enter A Valid Number\n";
+                    continue;
+                }
+
+                if (subchoice[0] == '4') break;
+
+                switch (subchoice[0]) {
+                case '1':
+                    cout << "Enter value: ";
+                    cin >> input;
+                    q.push(input);
+                    break;
+                case '2':
+                    cout << "Dequeued: " << q.pop() << "\n";
+                    break;
+                case '3':
+                    q.display();
+                    break;
+                }
+            }
+            break;
+
+        case '3':
+            flag = true;
+            break;
+        }
+    }
 
     return 0;
 }
